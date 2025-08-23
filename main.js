ca6db14ce77075ab60e6008d1be1188218dc4736
Jostyn07 <jostynag075@gmail.com>
2025-08-23 09:06:50 -0500

    Actualización 23/08/2005

---
+++ main.js
@@ -0,0 +1,6 @@
+const ventasBtn = document.getElementById("ventasbtn");
+if (ventasBtn) {
+    ventasBtn.addEventListener("click", function() {
+        window.location.href = "../Landign page/Landing page definitivo/index.html";
+    });
+}
